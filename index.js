6016c082e31edf3990cdd2727379fee719bdc938
wahyumsm <kurangwahyu@gmail.com>
2024-07-17 16:35:47 +0700

    push

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+  const data = {
+    message: "OK",
+    data: {
+      example: "Ini data JSON yang bisa kamu lihat",
+    },
+  };
+  res.json(data);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
