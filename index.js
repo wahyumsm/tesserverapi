677c86fe7479fa845a241afd17e03959ac3c3491
wahyumsm <kurangwahyu@gmail.com>
2024-07-17 16:30:34 +0700

    push

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+
+module.exports = app;
